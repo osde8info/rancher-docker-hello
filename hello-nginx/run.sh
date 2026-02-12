01b34c90593202046b7766f73b7ff1309c29e3e2
clive darra <cdarra@github.com>
2026-02-12 00:22:05 +0000

    add hello nextcloud

--- hello-nginx/run.sh
+++ hello-nginx/run.sh
@@ -1,7 +1,7 @@
 kubectl run ngx-hello-world --image=nginx-helloworld --image-pull-policy=Never --port=80
 kubectl port-forward pods/ngx-hello-world 8088:80
 
-curl localhost:8088
+curl http://localhost:8088
 
 # Remove the pod
 # kubectl delete pod ngxhello-world 
